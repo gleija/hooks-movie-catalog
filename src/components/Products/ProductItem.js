6bca7d581e4801a7cf30e3e4875e8ed17559a487
Gary Leija <gleija@seek.com.au>
2021-10-31 17:58:18 +1100

    Initial commit

---
+++ src/components/Products/ProductItem.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+
+import Card from '../UI/Card';
+import './ProductItem.css';
+import { ProductContext } from '../../context/products-context';
+
+
+const ProductItem = props => {
+  const context = useContext(ProductContext);
+  const toggleFavHandler = () => {
+    context.toggleFav(props.id);
+  };
+
+  return (
+    <Card style={{ marginBottom: '1rem' }}>
+      <div className="product-item">
+        <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
+        <p>{props.description}</p>
+        <button
+          className={!props.isFav ? 'button-outline' : ''}
+          onClick={toggleFavHandler}
+        >
+          {props.isFav ? 'Un-Favorite' : 'Favorite'}
+        </button>
+      </div>
+    </Card>
+  );
+};
+
+export default ProductItem;
